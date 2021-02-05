ab6f2cacfc5eca0050e936a8ad41e983c913c108
ptorngh <patrik.tornberg@hm.com>
2021-02-05 09:57:21 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int num = 10;
+	int result = increment(num);
+        System.out.println(result);
+
+
+
+	    }
+	    public static int increment(int n) {
+            return ++n;
+        }
+
+
+}
